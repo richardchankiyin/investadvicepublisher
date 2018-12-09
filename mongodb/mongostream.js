4121ee991c966fe966dea6b236c25068d572a584
Richard Chan <richardchankiyin@gmail.com>
2018-12-09 16:46:25 +0800

    update mongostream message to format csv (symbol,reportid,summary)

--- mongodb/mongostream.js
+++ mongodb/mongostream.js
@@ -4,6 +4,6 @@ const cursor = db.news.watch();
 while (!cursor.isExhausted()) {
    if (cursor.hasNext()) {
       doc = cursor.next().fullDocument;
-      print("output:" + doc.symbol);
+      print(doc.symbol + "," + doc.reportid + "," + doc.summary);
    }
 }
