9ebb33c13780addd4cc0b1bc751710baef21e85d
Richard Chan <richardchankiyin@gmail.com>
2018-12-09 15:48:50 +0800

    push news to scaledrone

--- mongodb/pushnews.sh
+++ mongodb/pushnews.sh
@@ -5,3 +5,5 @@ if [ "$#" -lt 1 ]; then echo "no input, bye!"; fi
 input=$1
 
 echo "news to be pushed: $input"
+
+curl -v -H "Content-Type: application/json" -X POST  -d "$input" https://api2.scaledrone.com/jiUv2Rpk1O8tZbZ7/notifications/publish
