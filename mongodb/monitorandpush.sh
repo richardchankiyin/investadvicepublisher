7b15d40cdb24ea20668ce8266e829fbd8fed8035
Richard Chan <richardchankiyin@gmail.com>
2018-12-09 20:26:16 +0800

    externalize monitorandpush.sh channelid

--- mongodb/monitorandpush.sh
+++ mongodb/monitorandpush.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+if [ ! -f channelid ]; then echo "no scaledrone channel id file"; exit -1; fi
+channel=$(cat channelid)
 log=mongostreamlog
-tail -0f $log | xargs -I {} ./pushnews.sh "{}"
+tail -0f $log | xargs -I {} ./pushnews.sh $channel "{}" 
--- mongodb/pushnews.sh
+++ mongodb/pushnews.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]; then echo "no input, bye!"; fi
+if [ "$#" -lt 2 ]; then echo "no channel id nor input, bye!"; fi
 
-input=$1
+channel=$1
+input=$2
 
 echo "news to be pushed: $input"
 
-curl -v -H "Content-Type: application/json" -X POST  -d "$input" https://api2.scaledrone.com/jiUv2Rpk1O8tZbZ7/notifications/publish
+curl -v -H "Content-Type: application/json" -X POST  -d "$input" https://api2.scaledrone.com/$channel/notifications/publish
