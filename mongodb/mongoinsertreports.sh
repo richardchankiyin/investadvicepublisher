b04c6449f5c9812c1ecd30f6ef98041735a98d78
richardchankiyin <richard.chan@analyzingstocks.com>
2018-12-14 17:12:37 +0800

    add sleep between inserting news and research

--- mongodb/mongoinsertreports.sh
+++ mongodb/mongoinsertreports.sh
@@ -10,6 +10,8 @@ mongo --port $port --eval "db=db.getSiblingDB('investadviceapp');db.report.inser
 
 mongo --port $port --eval "db=db.getSiblingDB('investadviceapp');db.news.insert({'symbol':'symbol$symbolseq', 'summary':'Firm XXX records profit growth 50% in YoY', 'reportid':'report$seq', 'time':ISODate()});"
 
+sleep 5;
+
 seq=$(date +%Y%m%d%H%M%s)
 mongo --port $port --eval "db=db.getSiblingDB('investadviceapp');db.report.insert({'symbol':'symbol$symbolseq','id':'report$seq','type':'research', 'detail':{'title':'Investment Bank ZZZ upgrades Firm XXX from buy to strong buy', 'author':'Mr AAA','content':'Firm XXX shows very strong profit growth in this year and this forms a strong trend in upcoming next couple years.'}});"
 
